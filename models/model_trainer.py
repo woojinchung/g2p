97d101280e9fd6180393d87c8a0e9e0ffbcc22e1
woojin <woojin@nyu.edu>
2017-12-07 13:28:13 -0500

    added evaluate_only mode & calculates word accuracy

--- models/model_trainer.py
+++ models/model_trainer.py
@@ -78,8 +78,10 @@ class ModelTrainer(object):
             labels_list = [label.cuda() for label in labels_list]
 
         loss = 0
-        correct = 0
-        preds = 0
+        total_correct = 0
+        total_guess = 0
+        total_wcorrect = 0
+        total_wguess = 0
 
         for i in range(len(outputs_list)):
             # calculate loss
@@ -88,10 +90,18 @@ class ModelTrainer(object):
 
             # calculate class accuracy
             pred = logits.data.max(1)[1].cpu() # get the index of the max log-probability
-            correct += pred.eq(labels_list[i].cpu().data).sum()
-            preds += labels_list[i].size(0)
+            correct = pred.eq(labels_list[i].cpu().data).sum()
+            guess = labels_list[i].size(0)
 
-        return loss, correct, preds
+            total_correct += correct
+            total_guess += guess
+
+            if correct == guess:
+                total_wcorrect += 1
+            
+            total_wguess += 1
+
+        return loss, total_correct, total_guess, total_wcorrect, total_wguess
 
     def run_batch(self, source_batch, labels_list, backprop):
         if self.FLAGS.gpu:
@@ -100,14 +110,14 @@ class ModelTrainer(object):
             input = source_batch[0].cpu()
 
         outputs_list = self.model.forward(input, source_batch[1])
-        loss, correct, preds = self.get_metrics(outputs_list, labels_list)
+        loss, correct, guess, wcorrect, wguess = self.get_metrics(outputs_list, labels_list)
 
         if backprop:
             self.backprop(loss)
         if self.FLAGS.gpu:
             loss = loss.cpu()
 
-        return loss, correct, preds
+        return loss, correct, guess, wcorrect, wguess
 
     def run_stage(self, epoch, backprop, stages_per_epoch, prints_per_stage):
         has_next = True
@@ -119,18 +129,23 @@ class ModelTrainer(object):
         stage_loss = 0
 
         total_correct = 0
-        total_pred = 0
+        total_guess = 0
+
+        total_wcorrect = 0
+        total_wguess = 0
 
         while has_next and n_batches < stage_batches:
             n_batches += 1
             source_batch, target_batch, has_next = epoch.get_new_batch()
-            loss, correct, preds = self.run_batch(source_batch, target_batch, backprop)
+            loss, correct, guess, wcorrect, wguess = self.run_batch(source_batch, target_batch, backprop)
             
             print_loss += loss
             stage_loss += loss
 
             total_correct += correct
-            total_pred += preds
+            total_guess += guess
+            total_wcorrect += wcorrect
+            total_wguess += wguess
 
             if n_batches % print_batches == 0:
                 self.print_stats(print_loss/print_batches)
@@ -138,9 +153,11 @@ class ModelTrainer(object):
         if prints_per_stage > 1:
             self.print_stats(stage_loss/n_batches)
 
-        class_acc = total_correct / float(total_pred)
+        class_acc = total_correct / float(total_guess)
+        wclass_acc = total_wcorrect / float(total_wguess)
 
-        print "class_acc:\t" + str(class_acc)
+        print "phoneme class_acc:\t" + str(class_acc)
+        print "word class_acc:\t" + str(wclass_acc)
 
         return stage_loss/n_batches, class_acc
 
@@ -155,7 +172,7 @@ class ModelTrainer(object):
             train_loss, train_acc = self.run_stage(train, True, self.FLAGS.stages_per_epoch, self.FLAGS.prints_per_stage)
             print("-------------validation-------------")
             valid = cdu.CorpusEpoch(self.dm.valid_pairs, self.dm, self.FLAGS.batch_size)
-            valid_loss, valid_acc = self.evaluate(valid)
+            valid_loss, valid_acc, wvalid_acc = self.evaluate(valid)
 
             if (1 - valid_acc) < 0.99 * best_dev_err and n_stages > 10:
                 best_dev_err = 1 - valid_acc
@@ -185,23 +202,29 @@ class ModelTrainer(object):
         has_next = True
         total_loss = 0
         total_correct = 0
-        total_pred = 0
+        total_guess = 0
+        total_wcorrect = 0
+        total_wguess = 0
 
         while has_next:
             source_batch, target_batch, has_next = epoch.get_new_batch()
-            loss, correct, preds = self.run_batch(source_batch, target_batch, False)
+            loss, correct, guess, wcorrect, wguess = self.run_batch(source_batch, target_batch, False)
             
             total_loss += loss
             total_correct += correct
-            total_pred += preds
+            total_guess += guess
+            total_wcorrect += wcorrect
+            total_wguess += wguess
 
-        class_acc = total_correct / float(total_pred)
         avg_loss = total_loss/epoch.n_batches
+        class_acc = total_correct / float(total_guess)
+        wclass_acc = total_wcorrect / float(total_wguess)
 
         self.print_stats(avg_loss)
-        print "class_acc:\t" + str(class_acc)
+        print "phoneme class_acc:\t" + str(class_acc)
+        print "word class_acc:\t" + str(wclass_acc)
 
-        return avg_loss, class_acc        
+        return avg_loss, class_acc, wclass_acc
 
     def start_up_print_and_logs(self):
         print("======================================================================")
@@ -216,26 +239,26 @@ class ModelTrainer(object):
 
 
     def run(self, epochs, stages, best_dev_err):
-        """The outer loop of the model trainer"""
-        self.start_up_print_and_logs()
-        n_epoch = epochs
-        n_stages = stages
-        n_stages_not_converging = 0
-        try:
-            while n_epoch < self.FLAGS.max_epochs:
-                print("===========================EPOCH %d=============================" % n_epoch)
-                n_stages_not_converging, n_stages, best_dev_err = self.run_epoch(n_stages_not_converging, n_epoch, n_stages, best_dev_err)
-                n_epoch += 1
-        except NotConvergingError:
-            # TODO: update
-            self.model.load_state_dict(torch.load(self.OUTPUT_PATH))
+        if self.FLAGS.evaluate_only is True:
             print("=====================TEST==================")
-            test_loss = self.run_stage(cdu.CorpusEpoch(self.dm.test_pairs, self.dm), False, 1, 1)
-#            self.LOGS.write("accuracy\t" + self.my_round(test_confusion.accuracy()) + "\n")
-        except KeyboardInterrupt:
-            print "\nSO LONG, AND THANKS FOR ALL THE FISH"
-        finally:
+            test = cdu.CorpusEpoch(self.dm.test_pairs, self.dm, self.FLAGS.batch_size)
+            test_loss, test_acc, wtest_acc = self.evaluate(test)
             self.LOGS.close()
+        else:
+            """The outer loop of the model trainer"""
+            self.start_up_print_and_logs()
+            n_epoch = epochs
+            n_stages = stages
+            n_stages_not_converging = 0
+            try:
+                while n_epoch < self.FLAGS.max_epochs:
+                    print("===========================EPOCH %d=============================" % n_epoch)
+                    n_stages_not_converging, n_stages, best_dev_err = self.run_epoch(n_stages_not_converging, n_epoch, n_stages, best_dev_err)
+                    n_epoch += 1
+            except KeyboardInterrupt:
+                print "\nSO LONG, AND THANKS FOR ALL THE FISH"
+            finally:
+                self.LOGS.close()
 
 
 class NotConvergingError(Exception):
